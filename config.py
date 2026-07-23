a62bf77ec70710273d3b5599d6448d22644f4681
agustin-eguiguren <agustin.egui@gmail.com>
2026-07-23 20:37:58 +0000

    Improve Config

--- config.py
+++ config.py
@@ -16,8 +16,9 @@ class Config(CommandLineManager):
         '''
         super().__init__()
 
-        self.parser = argparse.ArgumentParser(description="Open5Gs testing environment \
-                                                    setup for the TOPSSIM project", 
+        self.parser = argparse.ArgumentParser(description="TSIM aims to create a reproducible environment for testing 5G \
+                                            core performance within the TOPSSIM project. It uses a number of tools to automate \
+                                            the creation and configuration of virtual machines used for testing.", 
                                                     argument_default=argparse.SUPPRESS)
 
         self.addArguments()
@@ -113,44 +114,47 @@ class Config(CommandLineManager):
 
     def addArguments(self):
         # Actions
-        self.parser.add_argument("-destroy", action='store_true', help="Destroys all of the current VMs")
-        self.parser.add_argument("-restart", action='store_true', help="Destroys and restarts all of the current VMs")
-        self.parser.add_argument("-up", action='store_true', help="Just creates VMs and runs very basic setup with Infrastructure Manager (OpenTofu or Vagrant)")
-        self.parser.add_argument("-ansible", action='store_true', help="Calls Ansible to setup the VMs")
-        self.parser.add_argument("-test", action='store_true', help="Executes the commands from the run file")
-        self.parser.add_argument("-ssh", action='store_true', help="Outputs the public IPs and ports used to SSH into the machines.")
-        self.parser.add_argument("-ad_hoc", action='store_true', help="Uses Ansible ad-hoc commands to execute an action in the VMs")
-        self.parser.add_argument("-log", action='store_true', help="Uses Ansible ad-hoc commands to tail the logs of a OGS component")
+        actions_group = self.parser.add_mutually_exclusive_group()
+        actions_group.add_argument("-destroy", action='store_true', help="Destroys all of the current VMs")
+        actions_group.add_argument("-restart", action='store_true', help="Destroys and restarts all of the current VMs")
+        actions_group.add_argument("-up", action='store_true', help="Just creates VMs and runs very basic setup with Infrastructure Manager (OpenTofu or Vagrant)")
+        actions_group.add_argument("-ansible", action='store_true', help="Calls Ansible to setup the VMs")
+        actions_group.add_argument("-test", action='store_true', help="Executes the commands from the run file")
+        actions_group.add_argument("-ssh", action='store_true', help="Outputs the public IPs and ports used to SSH into the machines.")
+        actions_group.add_argument("-ad_hoc", action='store_true', help="Uses Ansible ad-hoc commands to execute an action in the VMs")
+        actions_group.add_argument("-log", action='store_true', help="Uses Ansible ad-hoc commands to tail the logs of a OGS component")
 
         # Printing (these stop execution)
-        self.parser.add_argument("-vultr_regions", action='store_true', help="Shows the available regions for Vultr")
-        self.parser.add_argument("-vultr_plans", action='store_true', help="Shows the available plans for Vultr")
-        self.parser.add_argument("-readme", action='store_true', help="Prints the README")
+        print_group = self.parser.add_mutually_exclusive_group()
+        print_group.add_argument("-vultr_regions", action='store_true', help="Shows the available regions for Vultr")
+        print_group.add_argument("-vultr_plans", action='store_true', help="Shows the available plans for Vultr")
+        print_group.add_argument("-readme", action='store_true', help="Prints the README")
         
         # General Arguments
-        self.parser.add_argument("-c", "--config", help="Gives the path to the config file that outlines all of the information necessary to configure the VMs")
-        self.parser.add_argument("-r", "--run", help="Gives the path to the a file that describes a sequence of commands to be run in the VMs")
-        self.parser.add_argument("-v", "--verbose", action='store_true', help="Gives the option to run a TUI that displays some logs alongside test execution")
-        self.parser.add_argument("--tui", action='store_true', help="Gives the option to run a TUI that displays some logs alongside test execution")
-        self.parser.add_argument("--provider", help="The VM provider that is used (Vultr, VirtualBox, VMWare, QEMU)")
-        self.parser.add_argument("--ogs_repo", help="The Open5GS repo that is installed to the VMs")
-        self.parser.add_argument("--ogs_version", help="The version (branch) of the Open5GS repo that is cloned")
-        self.parser.add_argument("--user_ssh_key", help="An ssh key automatically added to the authorized keys in the VMs")
-        self.parser.add_argument("--create_services", action='store_true', help="Creates service files for OGS components in /etc/system/systemd")
-        self.parser.add_argument("--copy_logs", action='store_true', help="Copies logs from VMs into local machine")
-        self.parser.add_argument("--capture_packets", action='store_true', help="Captures network packets from the VMs during testing and copies them over to the host machine")
-        self.parser.add_argument('--ansible_tags', nargs='+', help="Tells ansible which stages to run. Options: install_stage, config_stage, testing_stage, services_stage, ogstun, install_ogs")
+        general_group = self.parser.add_argument_group("General Arguments (override config file)")
+        general_group.add_argument("-c", "--config", help="Gives the path to the config file that outlines all of the information necessary to configure the VMs")
+        general_group.add_argument("-r", "--run", help="Gives the path to the a file that describes a sequence of commands to be run in the VMs")
+        general_group.add_argument("--tui", action='store_true', help="Gives the option to run a TUI that displays some logs alongside test execution")
+        general_group.add_argument("--provider", help="The VM provider that is used (Vultr, VirtualBox, VMWare, QEMU)")
+        general_group.add_argument("--user_ssh_key", help="An ssh key automatically added to the authorized keys in the VMs")
+        general_group.add_argument('--ansible_tags', nargs='+', help="Tells ansible which stages to run. Options: install_stage, config_stage, testing_stage, services_stage, ogstun, install_ogs")
+        general_group.add_argument("--create_services", action='store_true', help="Creates service files for OGS components in /etc/system/systemd")
+        general_group.add_argument("--copy_logs", action='store_true', help="Copies logs from VMs into local machine")
+        general_group.add_argument("--capture_packets", action='store_true', help="Captures network packets from the VMs during testing and copies them over to the host machine")
 
         # Vultr Arguments
-        self.parser.add_argument("--vultr_api_key", help="Personal Vultr API key")
+        vultr_group = self.parser.add_argument_group("Vultr Arguments (override config file)")
+        vultr_group.add_argument("--vultr_api_key", help="Personal Vultr API key")
 
         # Ad Hoc Arguments
-        self.parser.add_argument("--w", default="all", help="Which VMs to run ad-hoc commands on (all, hplmn, or vplmn), with a default value of all")
-        self.parser.add_argument("--m", default="ansible.builtin.shell", help="Ansible module to run ad-hoc command (default is ansible.builtin.shell)")
-        self.parser.add_argument("--a", help='A string of arguments for the ad-hoc command (e.g. "ip a")')
-        self.parser.add_argument("--B", default=-1, help='Timeout (in seconds) for ad-hoc command')
-        self.parser.add_argument("--P", default=-1, help='Poll time for ad-hoc commands')
+        adhoc_group = self.parser.add_argument_group("Arguments for Ad Hoc commands")
+        adhoc_group.add_argument("--w", default="all", help="Which VMs to run ad-hoc commands on (all, hplmn, or vplmn), with a default value of all")
+        adhoc_group.add_argument("--m", default="ansible.builtin.shell", help="Ansible module to run ad-hoc command (default is ansible.builtin.shell)")
+        adhoc_group.add_argument("--a", help='A string of arguments for the ad-hoc command (e.g. "ip a")')
+        adhoc_group.add_argument("--B", default=-1, help='Timeout (in seconds) for ad-hoc command')
+        adhoc_group.add_argument("--P", default=-1, help='Poll time for ad-hoc commands')
 
         # Logs
-        self.parser.add_argument("--func", nargs='+', help="Which function logs are going to be printed for (can be a list separated by spaces)")
-        self.parser.add_argument("--lines", default="10", help="The number of lines to be returned from logs")
+        logs_group = self.parser.add_argument_group("Arguments for capturing logs (with -log flag)")
+        logs_group.add_argument("--func", nargs='+', help="Which function logs are going to be printed for (can be a list separated by spaces)")
+        logs_group.add_argument("--lines", default="10", help="The number of lines to be returned from logs")
